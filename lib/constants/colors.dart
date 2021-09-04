e5ef60878805ea6f3a1eabc1fe54803ac2b95fa2
Justin DT <56988254+justindtran2001@users.noreply.github.com>
2021-09-05 01:19:13 +0700

    Created Theme

---
+++ lib/constants/colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const kEzLearnBlue600 = Color(0x3185FC);
+const kEzLearnBlue900 = Color(0x034FB9);
+
+const kEzLearnYellow400 = Color(0xF7CB15);
+
+const kEzLearnCorrectGreen = Color(0x12E667);
+
+const kEzLearnWrongRed = Color(0xE61212);
+const kEzLearnErrorRed = Color(0xFF0000);
+
+const kEzLearnMathPurple = Color(0xAF31FC);
+
+const kEzLearnPhysicsTeal = Color(0x00E3AA);
+
+const kEzLearnLiteratureOrange = Color(0xF5632F);
+
+const kEzLearnBlack = Color(0x191919);
+const kEzLearnWhite = Colors.white;
+const kEzLearnGrey = Color(0x838181);
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:final_cs426/routes/routes.dart';
-import 'package:final_cs426/screens/home_screen.dart';
 import 'package:final_cs426/screens/result_screen.dart';
 import 'package:final_cs426/screens/splash_screen.dart';
 import 'package:final_cs426/screens/test_choosing_screen.dart';
 import 'package:final_cs426/screens/test_screen.dart';
 import 'package:flutter/material.dart';
+import 'constants/colors.dart';
+
+final ThemeData _kEzLearnTheme = _buildEzLearnTheme();
 
 void main() {
   runApp(MyApp());
@@ -14,10 +16,104 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Raleway'),
+      title: 'EazyLearn',
+      // theme: _kEzLearnTheme,
+      theme: ThemeData(
+          primarySwatch: _kEzLearnTheme.primaryColor, fontFamily: 'Raleway'),
       routes: Routes.routes,
       initialRoute: Routes.home,
     );
   }
+}
+
+ThemeData _buildEzLearnTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kEzLearnBlue600,
+      primaryVariant: kEzLearnBlue900,
+      onPrimary: kEzLearnWhite,
+      secondary: kEzLearnYellow400,
+      onSecondary: kEzLearnBlack,
+      surface: kEzLearnWhite,
+      onSurface: kEzLearnBlack,
+      error: kEzLearnErrorRed,
+    ),
+    scaffoldBackgroundColor: kEzLearnWhite,
+    textTheme: _buildEzLearnTextTheme1(base.textTheme),
+    accentTextTheme: _buildEzLearnTextTheme2(base.textTheme),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 2.0,
+          color: kEzLearnBlue900,
+        ),
+      ),
+    ),
+  );
+}
+
+TextTheme _buildEzLearnTextTheme1(TextTheme base) {
+  return base
+      .copyWith(
+          headline4: base.headline4.copyWith(
+            fontWeight: FontWeight.w800,
+            fontSize: 36,
+            height: 1.2,
+            letterSpacing: 3,
+          ),
+          headline5: base.headline5.copyWith(
+            fontWeight: FontWeight.w700,
+            fontSize: 28,
+            height: 1.2,
+            letterSpacing: 1,
+          ),
+          headline6: base.headline6.copyWith(
+            fontWeight: FontWeight.w600,
+            fontSize: 18,
+            height: 1.4,
+          ),
+          subtitle1: base.subtitle1.copyWith(
+            fontWeight: FontWeight.w600,
+            fontStyle: FontStyle.italic,
+            fontSize: 14,
+            height: 1.4,
+            color: kEzLearnGrey,
+          ),
+          bodyText1: base.bodyText1.copyWith(
+            fontWeight: FontWeight.w600,
+            fontSize: 18,
+            height: 1.4,
+          ),
+          bodyText2: base.bodyText2.copyWith(
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+            height: 1.3,
+          ))
+      .apply(
+        fontFamily: 'Raleway',
+        displayColor: kEzLearnWhite,
+        bodyColor: kEzLearnBlack,
+      );
+}
+
+TextTheme _buildEzLearnTextTheme2(TextTheme base) {
+  return base
+      .copyWith(
+        headline5: base.headline5.copyWith(
+          fontWeight: FontWeight.w700,
+          fontSize: 24,
+          height: 1.2,
+          letterSpacing: 3,
+        ),
+        headline6: base.headline6.copyWith(
+          fontWeight: FontWeight.w600,
+          fontSize: 18,
+          height: 1.2,
+        ),
+      )
+      .apply(
+        fontFamily: 'OpenSans',
+        bodyColor: kEzLearnBlack,
+      );
 }
